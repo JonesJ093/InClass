ace2ce988b49ec0ffbe5ba0c0643b68910848052
Joshua Jones <JJones356@my.gcu.edu>
2020-01-08 14:49:39 -0700

    First Commit

---
+++ Hexagon area/Source.cpp
@@ -0,0 +1,16 @@
+//Joshua Jones
+//This is my own work
+//In class excercise 1/8/2020
+#include <iostream>
+using namespace std;
+int main() {
+	float kilogram,pound;
+	cout << "enter pound ";
+	cin >> pound;
+	kilogram = pound * .454;
+	cout << "pound=" << kilogram << " kg";
+
+
+
+return 0;
+}
